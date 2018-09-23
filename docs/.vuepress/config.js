560d1be176095e5e8828bcdb6ef4e83c97795d1a
DarynHolmes <DarynHolmes@gmail.com>
2018-09-23 14:48:39 +0100

    initial setup

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  title: 'Become a Developer in Test - C# Edition',
+  description: 'An introduction to test automation in C#'
+}
---
+++ docs/guide/README.md
@@ -0,0 +1,3 @@
+# Introduction
+
+<iframe width="100%" height="475" src="https://dotnetfiddle.net/Widget/CsCons" frameborder="0"></iframe>
