91296ed29e180d7687014012198f733aa5d31008
DarynHolmes <DarynHolmes@gmail.com>
2018-10-01 17:37:30 +0100

    added style

--- app/docs/.vuepress/config.js
+++ app/docs/.vuepress/config.js
@@ -4,7 +4,31 @@ module.exports = {
   title: 'Become a Developer in Test - C# Edition',
   description: 'This guide is an introduction to test automation in C#',
   markdown: {
-    lineNumbers: true
+    lineNumbers: true,
+    config: md => {
+      // use more markdown-it plugins!
+      md.use(require('markdown-it-container'), 'spoiler', {
+        validate: function(params) {
+          return params.trim().match(/^spoiler\s+(.*)$/);
+        },
+
+        render: function (tokens, idx) {
+          var m = tokens[idx].info.trim().match(/^spoiler\s+(.*)$/);
+
+          if (tokens[idx].nesting === 1) {
+            // opening tag
+            return '<details><summary>' + md.utils.escapeHtml(m[1]) + '</summary><div class="spoiler">\n';
+
+          } else {
+            // closing tag
+            return '</div></details>\n';
+          }
+        }
+      });
+
+      // console.log(md.render('::: spoiler click me\n*content*\n:::\n'));
+
+    }
   },
   themeConfig: {
     repo: 'https://github.com/DevAbout/dev-in-test-csharp',
